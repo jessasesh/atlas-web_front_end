275388161ae885b6c5c3f1919ab8ad8f84e8e625
jessasesh <jessica.dison@yahoo.com>
2024-07-10 17:54:39 -0500

    Closure and loops; adding iterator to populate number of students for each classroom

---
+++ Javascript_advanced/3-classrooms.js
@@ -0,0 +1,16 @@
+function createClassRoom(numbersOfStudents) {
+    function studentSeat(seat) {
+        return function () {
+            return seat;
+        };
+    }
+
+    let students = [];
+
+    for (let x = 0; x < numbersOfStudents; x++) {
+        students.push(studentSeat(x + 1));
+    }
+    return students;
+}
+
+const classRoom = createClassRoom(10);
