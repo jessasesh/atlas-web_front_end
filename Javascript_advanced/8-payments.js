c792a85211f62767e72be6e58726883576ebad25
jessasesh <jessica.dison@yahoo.com>
2024-07-10 18:55:48 -0500

    Stack order in functions; playing with some jquery

---
+++ Javascript_advanced/8-payments.js
@@ -0,0 +1,19 @@
+// For processing payment
+function processPayment(amount) {
+    console.log(`Collecting payment of ${amount}`);
+}
+
+// For processing order
+function processOrder(orderId, amount) {
+    console.log(`${orderId} is being processed`);
+    processPayment(amount);
+    console.log(`${orderId} has been fully processed`);
+}
+
+console.log("Processing orders");
+
+processOrder(12321, 10.99);
+processOrder(12322, 12.99);
+processOrder(12323, 15.0);
+
+console.log("All the orders have been processed");
