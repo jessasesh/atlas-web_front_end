b351339e118e3a17a53837d6acb190dd831bbbe0
jessasesh <jessica.dison@yahoo.com>
2024-07-10 17:49:41 -0500

    Closure; function and three variables, displays three alerts

---
+++ Javascript_advanced/2-function_me.js
@@ -0,0 +1,15 @@
+function welcomeMessage(fullName) {
+    alert('Welcome ' + fullName);
+}
+
+const guillaume = function () {
+    welcomeMessage('Guillaume');
+};
+
+const alex = function () {
+    welcomeMessage('Alex');
+};
+
+const fred = function () {
+    welcomeMessage('Fred');
+};
